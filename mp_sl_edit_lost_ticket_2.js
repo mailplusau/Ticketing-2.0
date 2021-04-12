2f28802dd92766280c2e131ceee6ee32ab2db21c
Sruti Desai <sruti.desai@student.unsw.edu.au>
2021-04-12 16:41:13 +1000

    2 files converted

---
+++ mp_sl_edit_lost_ticket_2.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @NApiVersion 2.0
+ * @NScriptType Suitelet
+ * 
+ * Description: 
+ * @Last Modified by: Sruti Desai
+ * 
+ */
+
+
+ define(['N/ui/serverWidget', 'N/email', 'N/runtime', 'N/search', 'N/record', 'N/http', 'N/log', 'N/redirect', 'N/format'], 
+ function(ui, email, runtime, search, record, http, log, redirect, format) {
+     var baseURL = 'https://1048144.app.netsuite.com';
+     if (runtime.EnvType == "SANDBOX") {
+         baseURL = 'https://1048144-sb3.app.netsuite.com';
+     }
+     var zee = 0;
+     var role = runtime.getCurrentUser().role;
+     if (role == 1000) {
+         //Franchisee
+         zee = runtime.getCurrentUser();
+     } 
+ 
+     function onRequest(context) {  
+         
+         if (context.request.method === 'GET') {
+             
+         } else {
+ 
+         }
+     }
+ 
+     function isNullorEmpty(strVal) {
+         return (strVal == null || strVal == '' || strVal == 'null' || strVal == undefined || strVal == 'undefined' || strVal == '- None -');
+     }
+     
+     return {
+         onRequest: onRequest
+     };
+ 
+ });
